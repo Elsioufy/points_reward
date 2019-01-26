528112b43008b2e835acf143afdd8f4fe92fd129
Hany Elsioufy <hany.elsioufy@gmail.com>
2019-01-26 22:56:56 +0200

    Change path after sign in/out devise action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(scope)
+    user_path(scope)
+  end
+
+  def after_sign_out_path_for(scope)
+    root_path
+  end
 end
