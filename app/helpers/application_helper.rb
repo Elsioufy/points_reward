0664a2ee343e9699b3b5dec2f81fe802418ae906
Hany Elsioufy <hany.elsioufy@gmail.com>
2019-01-27 01:29:58 +0200

    Add form errors helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def form_errors(resource)
+    return if resource.errors.empty?
+
+    return content_tag(:div, class: "alert alert-danger") do
+      content = ""
+      content << content_tag(:div, class: "header") do
+        "There were some errors saving this #{resource.class.name.titleize.downcase}"
+      end
+
+      content << content_tag(:ul) do
+        messages = ""
+        resource.errors.full_messages.each do |msg|
+          messages << content_tag(:li) do
+            msg
+          end
+        end
+
+        messages.html_safe
+      end
+
+      content.html_safe
+    end
+  end
 end
