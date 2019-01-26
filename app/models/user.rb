3c935ea4a4c2ab6461054b1f060d98e6b9203f2b
Hany Elsioufy <hany.elsioufy@gmail.com>
2019-01-26 22:54:01 +0200

    Add merit & devise gems and configure them

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_merit
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable
+
+end
