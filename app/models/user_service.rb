70e7af9a9cde8f079dae324e19956190a93112f2
Hany Elsioufy <hany.elsioufy@gmail.com>
2019-01-27 04:13:57 +0200

    Refactor user service model

--- app/models/user_service.rb
+++ app/models/user_service.rb
@@ -4,12 +4,13 @@ class UserService
     @user = user
     @level = 0
     @eq = 0.5
+    @category = 'Recommend points'
   end
 
   def compute_points
     @user = @user.parent
-    if @user.present? && (@user.points(category: 'Recommend points') < (1/(1-@eq)))
-      @user.add_points(@eq ** @level, category: 'Recommend points')
+    if @user.present? && (@user.points(category: @category) < (1/(1-@eq)))
+      @user.add_points(@eq ** @level, category: @category)
       @level += 1
       compute_points
     end
