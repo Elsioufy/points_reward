5ccc20f575cccc30844004d36a8be17cf6e5c7c5
Hany Elsioufy <hany.elsioufy@gmail.com>
2019-01-28 00:09:43 +0200

    Remove unwanted method in users repository

--- app/models/user_repository.rb
+++ app/models/user_repository.rb
@@ -4,10 +4,6 @@ class UserRepository
   def initialize(user_model)
     @model = user_model
   end
-  # return collection of users given their names
-  def get_users(names)
-    @model.where(name: names).order(:name)
-  end
   # return user model of specific user name
   def get_user(name)
     @model.find_by(name: name)
