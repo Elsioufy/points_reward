b487d1e6aa971becf5e8cb0d651be9c41b7f64fc
Hany Elsioufy <hany.elsioufy@gmail.com>
2019-01-27 06:46:51 +0200

    Add specs for user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    name { Faker::Name.name }
+    email { Faker::Internet.email(name) }
+    password { Faker::Internet.password 8,10 }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+  context 'Email is not valid' do
+    context 'Email is empty' do
+      it 'should not save the user' do
+        expect(build :user, email: '').not_to be_valid
+      end
+    end
+    context 'Email is not unique' do
+      it 'should not save the user' do
+        create(:user, email: 'dummy@example.com')
+        expect(build :user, email: 'dummy@example.com').not_to be_valid
+      end
+    end
+  end
+
+  context 'Name is not valid' do
+    context 'Name is empty' do
+      it 'should not save the user' do
+        expect(build :user, name: '').not_to be_valid
+      end
+    end
+    context 'Name is not unique' do
+      it 'should not save the email' do
+        create(:user, name: 'Dummy')
+        expect(build :user, name: 'Dummy').not_to be_valid
+      end
+    end
+  end
+  context 'Password is empty' do
+    it 'should not save the email' do
+      expect(build :user, password: '').not_to be_valid
+    end
+  end
+  context 'Email, Name and password are valid' do
+    it 'should save the user' do
+      expect(build(:user)).to be_valid
+      expect{create(:user)}.to change { User.count }.by(1)
+    end
+  end
 end
