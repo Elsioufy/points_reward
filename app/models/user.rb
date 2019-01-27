9d1640d9c6aad0330a758c7c43768534ad3c0e39
Hany Elsioufy <hany.elsioufy@gmail.com>
2019-01-27 04:54:55 +0200

    Permit sending name in sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-
+  validates :name, presence: true, uniqueness: true
   has_many :invitations, foreign_key: "inviter_user_id", dependent: :destroy
   belongs_to :parent, class_name: 'User', optional: true
   has_many :children, class_name: 'User', foreign_key: 'parent_id'
