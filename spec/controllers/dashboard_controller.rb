cb208d6e8fe76f3ceac2e8751db7bc1dd998f7e3
Hany Elsioufy <hany.elsioufy@gmail.com>
2019-01-28 02:05:50 +0200

    Add dashboard controller spec

---
+++ spec/controllers/dashboard_controller.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe InvitationsController, type: :controller do
+  context 'User signed in' do
+    before :each do
+      @me = create :user
+      sign_in @me
+    end
+    describe "GET new" do
+      it "has a 200 status code" do
+        get :indexs
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+  context 'No user signed in' do
+    describe "GET new" do
+      it "redirects to login path with 302" do
+        get :index
+        expect(response.status).to eq(302)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+end
