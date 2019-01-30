36057ba2c200795f9213f8a15ba1b53e4916b6e0
Hany Elsioufy <hany.elsioufy@gmail.com>
2019-01-30 23:57:52 +0200

    Add default mailers config for each env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -16,6 +16,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :sendmail
 
+  config.action_mailer.default_url_options = { host: 'clarkreward', port: 3000 }
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
